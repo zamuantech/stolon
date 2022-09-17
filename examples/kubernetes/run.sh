2e8bcb26d2db52fc936b2c76e7e580d5e7c29e4e
ghiyastfarisi <ghi.fai@gmail.com>
2022-09-17 20:04:35 +0700

    feat: move to pg-stolon

--- examples/kubernetes/run.sh
+++ examples/kubernetes/run.sh
@@ -1,7 +1,7 @@
 kubectl create namespace pg-stolon
 kubectl apply -f role-binding.yaml
 kubectl apply -f role.yaml
-kubectl run -i -t stolonctl --image=sorintlab/stolon:master-pg14 --restart=Never --rm -- /usr/local/bin/stolonctl --cluster-name=kube-stolon --store-backend=kubernetes --kube-resource-kind=configmap init
+kubectl run -n pg-stolon -i -t stolonctl --image=sorintlab/stolon:master-pg14 --restart=Never --rm -- /usr/local/bin/stolonctl --cluster-name=kube-stolon --store-backend=kubernetes --kube-resource-kind=configmap init --kube-namespace=pg-stolon
 kubectl create -f stolon-sentinel.yaml -n pg-stolon
 kubectl create -f secret.yaml -n pg-stolon
 kubectl create -f stolon-keeper.yaml -n pg-stolon
